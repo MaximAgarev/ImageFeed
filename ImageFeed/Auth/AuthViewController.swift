7b2f666944689386c1749cccd7a48f10bcdae892
Maxim Agarev <maximagarev@yandex.ru>
2023-02-17 15:43:10 +0300

    WebViewReady

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let AccessKey = "PS9-83s7sMRJqDYVrRq-2bUpYZhIZ1P3q665bAh3ThM"
+let SecretKey = "cc3mVU2hLCNI2MqL-vYX8UDWSBoFT6ff8DH5Y6gZqJY"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
